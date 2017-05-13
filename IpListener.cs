eb6aebf232e68497fae7d0ef9591697086173f37
Ramon de Klein <mail@ramondeklein.nl>
2017-05-13 16:41:24 +0200

    Initial version

---
+++ IpListener.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DeveloperProxy
+{
+    public class IpListener : IDisposable
+    {
+        private readonly TcpListener _tcpListener;
+        private readonly string _remoteHost;
+        private readonly int _remotePort;
+        private readonly CancellationTokenSource _ctsStop = new CancellationTokenSource();
+
+        public IpListener(IPEndPoint localEndPoint, string remoteHost, int remotePort)
+        {
+            _tcpListener = new TcpListener(localEndPoint);
+            _remoteHost = remoteHost;
+            _remotePort = remotePort;
+        }
+
+        public bool DecryptSsl { get; set; }
+        public bool IgnoreCertificateErrors { get; set; }
+        public X509Certificate Certificate { get; set; }
+
+        public void Dispose()
+        {
+            // Cancel all tasks and dispose
+            _ctsStop?.Cancel();
+            _ctsStop?.Dispose();
+        }
+
+        public async void Start()
+        {
+            _tcpListener.Start();
+            Console.WriteLine($"Listening on {_tcpListener.LocalEndpoint}.");
+
+            try
+            {
+                while (!_ctsStop.IsCancellationRequested)
+                {
+                    var socket = await _tcpListener.AcceptSocketAsync().ConfigureAwait(false);
+                    HandleConnection(socket, _ctsStop.Token);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // Expected
+            }
+        }
+
+        private async void HandleConnection(Socket socket, CancellationToken cancellationToken)
+        {
+            const int copyBlockSize = 64 * 1024;
+
+            // Obtain both streams
+            using (Stream localStream = new NetworkStream(socket, true))
+            {
+                // Encrypt use SSL
+                if (Certificate != null)
+                {
+                    var localSslStream = new SslStream(localStream);
+                    await localSslStream.AuthenticateAsServerAsync(Certificate).ConfigureAwait(false);
+                }
+
+                // Connect to the remote endpoint
+                using (var tcpClient = new TcpClient())
+                {
+                    // Dispose the TCP client when the operation is cancelled
+                    cancellationToken.Register(() => tcpClient.Dispose());
+
+                    // Attempt to connect
+                    await tcpClient.ConnectAsync(_remoteHost, _remotePort).ConfigureAwait(false);
+                    var remoteStream = (Stream)tcpClient.GetStream();
+
+                    if (DecryptSsl)
+                    {
+                        // Create the SSL stream with certificate validation
+                        var remoteSslStream = new SslStream(remoteStream, false, (sender, certificate, chain, errors) =>
+                        {
+                            // No need for checking if there are no errors
+                            if (errors == SslPolicyErrors.None)
+                                return true;
+
+                            // Log warning
+                            Console.WriteLine($"Certificate with subject '{certificate.Subject}' has error {errors}.");
+                            return IgnoreCertificateErrors;
+                        });
+                        await remoteSslStream.AuthenticateAsClientAsync(_remoteHost);
+
+                        // Use the SSL stream
+                        remoteStream = remoteSslStream;
+                    }
+
+                    // Wait until the streams have completed
+                    await Task.WhenAny(
+                        localStream.CopyToAsync(remoteStream, copyBlockSize, cancellationToken),
+                        remoteStream.CopyToAsync(localStream, copyBlockSize, cancellationToken)).ConfigureAwait(false);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace DeveloperProxy
+{
+    public static class Program
+    {
+        private static int Main(string[] args)
+        {
+            var app = new CommandLineApplication
+            {
+                Name = Assembly.GetEntryAssembly().GetName().Name,
+                Description = "TCP proxy service for development purposes."
+            };
+
+            // Specify the help option
+            app.HelpOption("-?|-h|--help");
+
+            // Add some options
+            var optionListenAll = app.Option("-a|--listen-all", "Listen on all IP endpoints.", CommandOptionType.NoValue);
+            var optionListenEndpoint = app.Option("-e|--listen-endpoint<ip-address>", "Listen on the specific IP endpoint", CommandOptionType.MultipleValue);
+            var optionDecryptSsl = app.Option("-d|--decrypt-ssl", "Decrypt the remote SSL stream", CommandOptionType.NoValue);
+            var optionIgnoreCertError = app.Option("-i|--ignore-certificate-errors", "Ignore any certificate errors", CommandOptionType.NoValue);
+            var optionUseCertificate = app.Option("-c|--use-certificate", "Use the specified certificate", CommandOptionType.SingleValue);
+            var optionCertificatePassword = app.Option("--certificate-password", "Use the specified certificate password", CommandOptionType.SingleValue);
+            var optionLocalPort = app.Option("-l|--local-port", "Expose the service on the specified local port", CommandOptionType.SingleValue);
+            var optionHost = app.Option("-h|--host", "Connect to the service on the specified host", CommandOptionType.SingleValue);
+            var optionRemotePort = app.Option("-p|--remote-port", "Connect to the service on the specified port", CommandOptionType.SingleValue);
+
+            app.OnExecute(() => {
+                // Determine the IP end-points where to listen to
+                IEnumerable<IPAddress> ipAddresses;
+                if (optionListenAll.HasValue())
+                    ipAddresses = new[] {IPAddress.Any};
+                else if (optionListenEndpoint.HasValue())
+                    ipAddresses = optionListenEndpoint.Values.Select(IPAddress.Parse);
+                else
+                    ipAddresses = GetIpAddresses(true);
+
+                // Determine the endpoints
+                var localPort = optionLocalPort.HasValue() ? int.Parse(optionLocalPort.Value()) : 0;
+                var host = optionHost.HasValue() ? optionHost.Value() : null;
+                var remotePort = optionRemotePort.HasValue() ? int.Parse(optionRemotePort.Value()) : 0;
+
+                // Load the certificate (if specified)
+                X509Certificate2 certificate = null;
+                if (optionUseCertificate.HasValue())
+                {
+                    var password = optionCertificatePassword.HasValue() ? optionCertificatePassword.Value() : null;
+                    certificate = new X509Certificate2(optionUseCertificate.Value(), password);
+                    if (!certificate.HasPrivateKey)
+                        throw new Exception("Certificate doesn't have a private key.");
+                }
+
+                // Create all the listeners
+                var listeners = ipAddresses.Select(ip => new IpListener(new IPEndPoint(ip, localPort), host, remotePort)
+                {
+                    DecryptSsl = optionDecryptSsl.HasValue(),
+                    IgnoreCertificateErrors = optionIgnoreCertError.HasValue(),
+                    Certificate = certificate
+                }).ToList();
+
+                // Start all listeners
+                foreach (var listener in listeners)
+                    listener.Start();
+
+                // Keep running until the task is cancelled
+                WaitForCtrlC();
+                
+                // Dispose all listeners
+                foreach (var listener in listeners)
+                    listener.Dispose();
+
+                return 0;
+            });
+
+            return app.Execute(args);
+        }
+
+        private static void WaitForCtrlC()
+        {
+            var taskCompleted = new TaskCompletionSource<bool>();
+            Console.CancelKeyPress += (sender, eventArgs) => taskCompleted.TrySetResult(true);
+            taskCompleted.Task.Wait();
+        }
+
+        private static IEnumerable<IPAddress> GetIpAddresses(bool localOnly)
+        {
+            foreach (var networkInterface in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (!localOnly || networkInterface.NetworkInterfaceType == NetworkInterfaceType.Loopback)
+                {
+                    var ipProperties = networkInterface.GetIPProperties();
+                    foreach (var unicastAddress in ipProperties.UnicastAddresses)
+                        yield return unicastAddress.Address;
+                }
+            }
+        }
+    }
+}
